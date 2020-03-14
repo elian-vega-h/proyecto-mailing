b44e86dd63a56df620a03b01001bfb1a908b55b2
elian-vega-h <50813006+elian-vega-h@users.noreply.github.com>
2020-03-13 23:29:43 -0500

    mailing update

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,15 +1,11 @@
 import React from "react";
+import image from "../images/advertising.jpg";
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <img
-          src="https://i.pinimg.com/564x/91/89/19/918919d87d5b50cf0741281f4d119642.jpg"
-          alt="Smiley face"
-          height="700vh"
-          width="700vh"
-        />
+        <img src={image} alt="Smiley face" height="700vh" width="700vh" />
       </div>
     );
   }
